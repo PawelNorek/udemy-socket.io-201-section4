d424cba6bd3c08165ead9aa1081c2599bcdaf6ab
pawel <pawel@4nol74.cloud>
2023-09-04 22:07:45 +0200

    dynamic room DOM population

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -15,6 +15,23 @@ socket.on('nsList', nsData => {
 	console.log(nsData)
 	const nameSpacesDiv = document.querySelector('.namespaces')
 	nsData.forEach(ns => {
-		nameSpacesDiv.innerHTML += `<div class="namespace" ns='${ns.name}'><img src='${ns.image}'></div>`
+		nameSpacesDiv.innerHTML += `<div class="namespace" ns='${ns.endpoint}'><img src='${ns.image}'></div>`
+	})
+
+	Array.from(document.getElementsByClassName('namespace')).forEach(element => {
+		console.log(element)
+		element.addEventListener('click', e => {
+			const nsEndpoint = element.getAttribute('ns')
+			console.log(nsEndpoint)
+
+			const clickedNs = nsData.find(row => row.endpoint === nsEndpoint)
+			const rooms = clickedNs.rooms
+
+			let roomList = document.querySelector('.room-list')
+			roomList.innerHTML = ''
+			rooms.forEach(room => {
+				roomList.innerHTML += `<li><span class="glyphicon glyphicon-lock"></span>${room.roomTitle}</li>`
+			})
+		})
 	})
 })
