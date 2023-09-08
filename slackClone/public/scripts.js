31e8590f8926c7ba9b184931d3de3f4914f8af44
pawel <pawel@4nol74.cloud>
2023-09-08 21:41:46 +0200

    UX cleanup done

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -12,26 +12,21 @@ socket.on('connect', () => {
 })
 
 socket.on('nsList', nsData => {
-	console.log(nsData)
+	const lastNs = localStorage.getItem('lastNs')
 	const nameSpacesDiv = document.querySelector('.namespaces')
+	nameSpacesDiv.innerHTML = ''
 	nsData.forEach(ns => {
 		nameSpacesDiv.innerHTML += `<div class="namespace" ns='${ns.endpoint}'><img src='${ns.image}'></div>`
 	})
 
 	Array.from(document.getElementsByClassName('namespace')).forEach(element => {
-		console.log(element)
 		element.addEventListener('click', e => {
-			const nsEndpoint = element.getAttribute('ns')
-			console.log(nsEndpoint)
-
-			const clickedNs = nsData.find(row => row.endpoint === nsEndpoint)
-			const rooms = clickedNs.rooms
-
-			let roomList = document.querySelector('.room-list')
-			roomList.innerHTML = ''
-			rooms.forEach(room => {
-				roomList.innerHTML += `<li><span class="glyphicon glyphicon-lock"></span>${room.roomTitle}</li>`
-			})
+			joinNs(element, nsData)
 		})
 	})
+
+	joinNs(
+		lastNs ? document.querySelector('[ns="' + lastNs + '"]') : document.getElementsByClassName('namespace')[0],
+		nsData
+	)
 })
